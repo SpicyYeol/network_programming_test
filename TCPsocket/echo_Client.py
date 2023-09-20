15f6a6fd2d364fc4d01b2dca23100c00f1f5d756
Dae Yeol Kim <spicyyeol@gmail.com>
2023-09-20 20:42:32 +0900

    try except 예외처리 clienr

--- TCPsocket/echo_Client.py
+++ TCPsocket/echo_Client.py
@@ -6,6 +6,23 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(address) # 서버 연결 요청
 while True:
 	msg = input("Message to send: ")
-	s.send(msg.encode()) #send a message to server
-	data = s.recv(BUFSIZE) #receive message from server
-	print("Received message: %s" %data.decode()) # 바이트형을 문자열로 변환
+
+	if not msg:
+		continue
+	try:
+		s.send(msg.encode()) #send a message to server
+	except:
+		print("연결 종료")
+		break
+
+	try: # 데이터 읽기
+		data = s.recv(BUFSIZE) #receive message from server
+		if not data:
+			print("연결 종료")
+			break
+		print("Received message: %s" %data.decode()) # 바이트형을 문자열로 변환
+	except:# 연결 종료
+		print("연결 종료")
+		break
+s.close()
+
